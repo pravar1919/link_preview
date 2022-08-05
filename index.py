75b1aa497f61fc5980a5c063e2f563d3086122eb
pravar18 <pravar.sharma@gmail.com>
2022-08-05 20:23:54 +0530

    commit

--- index.py
+++ index.py
@@ -1,27 +1,44 @@
 import httpx, json
 from bs4 import BeautifulSoup
 
-def main(url):
-    url=url
+def lambda_handler(event):
+    # print(event['queryStringParameters']['url'])
+    url=event#['queryStringParameters']['url']
     if not url.startswith("https://") or url.startswith("http://"):
-        return {"detail":"Invalid Url"}
+        return {"status":400, "detail":"Invalid Url Try using https:// before link"}
     with httpx.Client() as client:
         r = client.get(url).text
         soup = BeautifulSoup(r, features="html.parser")
-
-        title = soup.title.string
+    
+        # print("soup: ",soup)
+        
+        title = soup.find('meta', attrs={'title': 'og:title'})
+        if title:
+            title = title['content']
+        else:
+            title = ""
         # print ('TITLE IS :', title)
 
         image = soup.find('meta', attrs={'property': 'og:image'})
-        image = image['content']
+        if image:
+            image = image['content']
+        else:
+            image = ""
         # print("IMAGE IS: ", image['content'])
         meta = soup.find_all('meta')
+        print(soup.find('meta', attrs={'name': 'description'}))
         for tag in meta:
             if 'name' in tag.attrs.keys() and tag.attrs['name'].strip().lower() in ['description', 'keywords']:
                 # print ('NAME    :',tag.attrs['name'].lower())
                 desc = tag.attrs['content']
                 # print ('CONTENT :',tag.attrs['content'])
-
+        try:
+            desc = desc
+        except:
+            desc = ""
+        print("desc",desc)
+        # if not desc:
+        #     desc = ""
         data = {
             "title":title,
             "description":desc,
@@ -29,6 +46,7 @@ def main(url):
             "url":url
         }
         print(data)
-        return {data}
+        return {"detail":json.dumps(data), "status":200}
 
-main("https://www.ambitionbox.com/overview/trootech-business-solutions-overview?utm_campaign=share_overview_header&utm_medium=company_header&utm_source=ambitionbox")
+# lambda_handler('https://app.minstein.com')
+lambda_handler('https://www.facebook.com')
